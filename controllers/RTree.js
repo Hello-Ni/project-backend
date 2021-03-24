7e5d5461b98c2e2a6f298249f98fbe685f7c2960
zzzz20102xx <zz20102aa@gmail.com>
2021-03-25 06:40:59 +0800

    finish the insert rtree interface

--- controllers/RTree.js
+++ controllers/RTree.js
@@ -6,9 +6,9 @@ class Instruction{
         let Rtree=new RTree()
          
         locations.forEach((loc,idx) => {
-            let flag= Rtree.insertTree(rootBound,loc.lng,loc.lat,"point")
+            let flag= Rtree.insertTree(rootBound,loc.lng,loc.lat,idx,"point")
         });
-        console.log(rootBound)
+        //console.log(rootBound)
         return rootBound
     }
 
@@ -28,17 +28,17 @@ class Instruction{
             let number=bounds.number;
             if(lng>parseFloat(location.left) && lng<parseFloat(location.right) 
                && lat<parseFloat(location.top) && lat>parseFloat(location.bottom))
-                marks.push({lng:lng,lat:lat,number:number,type:"dog"})
+                marks.push({lng:lng,lat:lat,number:number})
         }
 
 
     }
-    insert(bounds,location,type){
-
+    insert(bounds,location,index){
+        console.log(index)
         let Rtree=new RTree()
         let x=parseFloat(location.longitude)
         let y=parseFloat(location.latitude)
-        Rtree.insertTree(bounds,x,y,"point")
+        Rtree.insertTree(bounds,x,y,index,"point")
         return bounds
     }
     visit(bounds){
@@ -103,7 +103,7 @@ class Instruction{
         }
         //insert
         let point=new Bound()
-        point.setBoundary({left:x,right:x,top:y,bottom:y})
+        point.boundary={left:x,right:x,top:y,bottom:y}
         point.number=(++maxIdx)
         parent.nodes.push(point.number)
         if(parent.nodes.length>=parent.maxContain)
@@ -144,14 +144,14 @@ class Instruction{
     }
 }
 class RTree{
-    insertTree(bounds,x,y,type){
+    insertTree(bounds,x,y,number,type){
         if(bounds.contain_type==="point"){
             //insert in many point
             
             let point=new Bound();
-            point.setBoundary({left:x,right:x,top:y,bottom:y} )
+            point.boundary={left:x,right:x,top:y,bottom:y}
             //console.log(point.boundary)
-            //point.number=number;
+            point.number=number;
             bounds.children.push(point);
         }else{
             //insert in many rectangle
@@ -184,7 +184,7 @@ class RTree{
                 }
                 
             })
-            if(this.insertTree(bounds.children[idx],x,y,type)){
+            if(this.insertTree(bounds.children[idx],x,y,number,type)){
                 let newChildren=this.getSplitChildren(bounds.children[idx]);
                 bounds.children.splice(idx,1);
                 newChildren.forEach((child)=>{
@@ -254,7 +254,7 @@ class RTree{
                     let new_child=bounds.children[j];
                     b1.children.push(new_child);                    
                 }
-                b1.setBoundary(new_bdy)
+                b1.boundary=new_bdy
                 perimeter=(Math.abs(new_bdy.right-new_bdy.left)+Math.abs(new_bdy.top-new_bdy.bottom));
                 new_bdy={left:180,right:0,top:0,bottom:90};
                 for(let j=i ;j<bounds.maxContain;++j){
@@ -274,7 +274,7 @@ class RTree{
                     let new_child=bounds.children[j];
                     b2.children.push(new_child);
                 }
-                b2.setBoundary(new_bdy)
+                b2.boundary=new_bdy
                 perimeter+=(Math.abs(new_bdy.right-new_bdy.left)+Math.abs(new_bdy.top-new_bdy.bottom));
                 
                 if(perimeter < total_perimeter){
@@ -343,209 +343,6 @@ class Bound{
         this.number=0;
 
     }
-    initial(bounds){
-        this.type=bounds.type
-        this.children=bounds.children
-        this.nodes=bounds.node
-        this.boundary=bounds.boundary
-        this.contain_type=bounds.contain_type
-        this.maxContain=bounds.maxContain
-        this.number=bounds.number
-    }
-    setBoundary(new_boundary){
-        this.boundary=Object.assign(new_boundary)
-    }
-    insert(x,y,type){
-        if(this.contain_type==="point"){
-            //insert in many point
-            
-            let point=new Bound();
-            point.setBoundary({left:x,right:x,top:y,bottom:y} )
-            //point.number=number;
-            this.children.push(point);
-        }else{
-            //insert in many rectangle
-            let min_d_height=1000;
-            let min_d_width=1000;
-            let idx=0;
-
-            //find the closer bound
-            this.children.forEach((child,i)=>{
-                let d_w = 0;
-                let d_h = 0;
-                if (x < child.boundary.left) {
-                    d_w += (child.boundary.left - x);
-                   
-                }
-                if (x > child.boundary.right) {
-                    d_w += (x - child.boundary.right);
-                }
-                if (y > child.boundary.top) {
-                    d_h += (y-child.boundary.top);
-                }
-                if (y < child.boundary.bottom ) {
-                    d_h += (child.boundary.bottom-y);
-                }
-                if (d_w + d_h < min_d_width + min_d_height) {
-                    min_d_width = d_w;  
-                    min_d_height = d_h;
-                    idx = i;
-                   
-                }
-                
-            })
-            if(this.children[idx].insert(x,y,type)){
-                let newChildren=this.children[idx].getSplitChildren();
-                this.children.splice(idx,1);
-                newChildren.forEach((child)=>{
-                    this.children.push(child);
-                }) 
-            }
-                
-
-        }
-        //expand rectangle
-        if(x < this.boundary.left)this.boundary.left=x;    
-        if(x > this.boundary.right)this.boundary.right=x;
-        if(y > this.boundary.top)this.boundary.top=y;
-        if(y <this.boundary.bottom)this.boundary.bottom=y;
-        if(this.isOverFlow()){
-            return true;
-        }   
-        else{
-            return false
-        }
-    }
-    isOverFlow(){
-        if(this.children.length>=this.maxContain){
-            if (this.contain_type == "point") {
-                
-                //find minimum perimeter with top/left sorting
-                this.minBound(2);
-                
-            } else {
-                //find munimum perimeter with all boundary sorting
-                this.minBound(4);
-            }
-            return true
-        }
-        else
-            return false
-    }
-    minBound(cmp_time){
-        let new_b1 = new Bound();
-        let new_b2 = new Bound();
-        let new_bdy={left:180,right:0,top:0,bottom:90};
-        let total_perimeter=1000;
-        let minB=this.maxContain*0.4;
-        for(let k=0;k<cmp_time;++k){  
-            this.sortByDir(k);
-            //min 0.4B max B point
-            for(let i=minB ; i<=this.maxContain-minB ; ++i){
-                new_bdy={left:180,right:0,top:0,bottom:90};
-                let perimeter=0;
-                let b1=new Bound();
-                let b2=new Bound();
-                for(let j=0;j<i;++j){     
-                    //find the new boundary
-                    let top=this.children[j].boundary.top;
-                    let left=this.children[j].boundary.left;
-                    let btm=this.children[j].boundary.bottom;
-                    let right=this.children[j].boundary.right;
-                    if(top> new_bdy.top)
-                        new_bdy.top=top;
-                    if(btm <new_bdy.bottom)
-                        new_bdy.bottom=btm;
-                    if(left < new_bdy.left)
-                        new_bdy.left=left;
-                    if(right > new_bdy.right)
-                        new_bdy.right=right;
-                    //console.log("top:"+new_bdy.top+" left:"+new_bdy.left+" right:"+new_bdy.right+" bottom:"+new_bdy.bottom)
-                    let new_child=this.children[j];
-                    b1.children.push(new_child);                    
-                }
-                b1.setBoundary(new_bdy);
-                perimeter=(Math.abs(new_bdy.right-new_bdy.left)+Math.abs(new_bdy.top-new_bdy.bottom));
-                new_bdy={left:180,right:0,top:0,bottom:90};
-                for(let j=i ;j<this.maxContain;++j){
-                    //find the new boundary
-                    let top=this.children[j].boundary.top;
-                    let left=this.children[j].boundary.left;
-                    let btm=this.children[j].boundary.bottom;
-                    let right=this.children[j].boundary.right;
-                    if(top> new_bdy.top)
-                        new_bdy.top=top;
-                    if(btm <new_bdy.bottom)
-                        new_bdy.bottom=btm;
-                    if(left < new_bdy.left)
-                        new_bdy.left=left;
-                    if(right > new_bdy.right)
-                        new_bdy.right=right;
-                    let new_child=this.children[j];
-                    b2.children.push(new_child);
-                }
-                b2.setBoundary(new_bdy);
-                perimeter+=(Math.abs(new_bdy.right-new_bdy.left)+Math.abs(new_bdy.top-new_bdy.bottom));
-                
-                if(perimeter < total_perimeter){
-                    
-                    try {
-                        new_b1=Object.assign(b1)
-                        new_b2=Object.assign(b2)
-
-                    } catch (error) {
-                        console.log(error)
-                    }
-                    total_perimeter=perimeter
-                }
-            }
-        }
-        this.children=[];
-        if(this.contain_type=="point"){
-            new_b1.contain_type="point"
-            new_b2.contain_type="point"
-            this.contain_type="rectangle"
-        }
-        else{
-            new_b1.contain_type="rectangle"
-            new_b2.contain_type="rectangle"
-        }
-        this.children.push(new_b1)
-        this.children.push(new_b2)
-        //console.log(this.children)
-
-    }
-    sortByDir(boundary){
-        switch(boundary){
-            case 0:
-                this.children.sort((a,b)=>{
-                    return a.boundary.left-b.boundary.left
-                })
-                break;
-            case 1:       
-                this.children.sort((a,b)=>{
-                    return a.boundary.top-b.boundary.top
-                })
-                break;
-            case 2:       
-                this.children.sort((a,b)=>{
-                    return a.boundary.right-b.boundary.right
-                })
-            break;
-            case 3:       
-                this.children.sort((a,b)=>{
-                    return a.boundary.bottom-b.boundary.bottom
-                })
-            break;
-        }
-    }
-    getSplitChildren(){
-        return this.children;
-    }
-    
-
-    
-
 }
 let instruction=new Instruction();
 module.exports={
--- controllers/mapController.js
+++ controllers/mapController.js
@@ -1,19 +1,29 @@
 const fs=require('fs');
 const path=require('path');
-const MapData = require('../models/Map');
+//const MapData = require('../models/Map');
 const { all } = require('../routers/MapRouters');
 const RTree=require('./RTree');
 
 const mapSearch = (req,res)=>{
     try {
+        //console.log(req.body)
         let allBound=fs.readFileSync(path.join(__dirname,'../public/RTree.json'),'utf-8')
+        let animalData=fs.readFileSync(path.join(__dirname,'../public/Animal.json'),'utf-8')
         allBound=JSON.parse(allBound)
+        animalData=JSON.parse(animalData)
         let location=req.body
-        let nearMark=[];
-        RTree.instruction.search(allBound,location,nearMark)
+        let nearMarks=[];
+        RTree.instruction.search(allBound,location,nearMarks)
         // let root=allBound.find(bound=>bound.type==="root")
         // RTree.instruction.searchDataBaseTree(allBound,root,location,nearMark)
-        res.send({data:nearMark})
+        nearMarks.forEach((mark,idx)=>{
+            let result=animalData.find((data)=>data.number===mark.number)
+            delete result.number
+            nearMarks[idx]={...mark,...result}
+
+        })
+        //console.log(nearMarks)
+        res.send({data:nearMarks})
     } catch (error) {
         console.log(error)
     }
@@ -21,29 +31,42 @@ const mapSearch = (req,res)=>{
 }
 const mapInsert = (req,res)=>{  
     try {
-        let allBound=fs.readFileSync(path.join(__dirname,'../public/RTree.json'),'utf-8')
-        allBound=JSON.parse(allBound)
-        let location=req.body
-        let newBounds=RTree.instruction.insert(allBound,location)
-        let writeData=JSON.stringify(newBounds,null,"\t")
-        fs.writeFileSync(path.join(__dirname,'../public/RTree.json'),writeData,'utf-8')
+        console.log("insert")
+        let allBoundFileData=fs.readFileSync(path.join(__dirname,'../public/RTree.json'),'utf-8')
+        let animalFileData=fs.readFileSync(path.join(__dirname,'../public/Animal.json'),'utf-8')
+        let allBound=JSON.parse(allBoundFileData)
+        let animalData=JSON.parse(animalFileData)
+        let new_detail=JSON.parse(req.body.detail)
+        new_detail["number"]=allBound.nodesNum
+        animalData.push(new_detail)
+        let location=JSON.parse(req.body.location)
+        let newBounds=RTree.instruction.insert(allBound,location,allBound.nodesNum++)
+       
+        let writeTree=JSON.stringify(newBounds,null,"\t")
+        let writeAnimal=JSON.stringify(animalData,null,"\t")
+        fs.writeFileSync(path.join(__dirname,'../public/RTree.json'),writeTree,'utf-8')
+        fs.writeFileSync(path.join(__dirname,'../public/Animal.json'),writeAnimal,'utf-8')
         // let root=allBound.find(bound=>bound.type==="root")
         // RTree.instruction.insertDataBaseTree(allBound,allBound.length,root,location);
         res.send({status:'OK'})
         
     } catch (error) {
-        
+        res.send({status:'fail'})
     }
 }
 const mapCreate=(req,res)=>{
 
     try {
+        console.log("reset")
         const data=fs.readFileSync(path.join(__dirname,'../public/location.json'),'utf-8');
+        let animal_data=fs.readFileSync(path.join(__dirname,'../public/Animal.json'),'utf-8')
+        
         let locations=JSON.parse(data);
+        let animal=JSON.parse(animal_data)
         let roots=RTree.instruction.created(locations);
-
-
-
+        roots["nodesNum"]=locations.length
+        
+        animal=animal.slice(0,3)
         /** for not recursive solve */
         //let total=RTree.instruction.visit(roots)
         // let allNode=RTree.instruction.allNode
@@ -60,8 +83,9 @@ const mapCreate=(req,res)=>{
         // });
 
         let writeData=JSON.stringify(roots,null,"\t")
+        let writeAnimal=JSON.stringify(animal,null,"\t")
         fs.writeFileSync(path.join(__dirname,'../public/RTree.json'),writeData,'utf-8')
-        
+        fs.writeFileSync(path.join(__dirname,'../public/Animal.json'),writeAnimal,'utf-8')
         res.send({status:1})
     } catch (error) {
         res.send({status:0})
